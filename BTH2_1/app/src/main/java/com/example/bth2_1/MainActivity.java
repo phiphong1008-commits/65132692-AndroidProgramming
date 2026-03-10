1006b11398ad6d5c3b0474c93b2516d1b6753434
phiphong1008-commits <phiphong1008@gmail.com>
2026-03-10 13:31:05 +0700

    Hoàn thiện code

--- BTH2_1/app/src/main/java/com/example/bth2_1/MainActivity.java
+++ BTH2_1/app/src/main/java/com/example/bth2_1/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.bth2_1;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -20,5 +22,21 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+    }
+    public void XuLyCong(View view){
+        EditText soA = findViewById(R.id.edtA);
+        EditText soB = findViewById(R.id.edtB);
+        EditText ketqua = findViewById(R.id.edtKQ);
+
+        String strA = soA.getText().toString();
+        String strB = soB.getText().toString();
+
+        int so_A = Integer.parseInt(strA);
+        int so_B = Integer.parseInt(strB);
+
+        int kq = so_A + so_B;
+
+        String strkq = String.valueOf(kq);
+        ketqua.setText(strkq);
     }
 }
